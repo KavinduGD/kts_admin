3028394b34b728462c6131aa3945a9fb4b86fad0
Kavindu Gihan <kavidudharmasiri90@gmail.com>
2026-02-09 20:21:11 +0530

    pipeline check 2

--- src/pages/loginpage/index.jsx
+++ src/pages/loginpage/index.jsx
@@ -127,7 +127,7 @@ function Login() {
           <div className="font-roboto_slab text-[20px] xsm:text-[25px]  md:text-[25px] sm:text-[30px] font-bold flex flex-col items-center leading-[92%] text-center">
             <p>International</p>
             <p>Academic Institution</p>
-            <p>Transport Management System pipeline check</p>
+            <p>Transport Management System pipeline check 2</p>
           </div>
         </div>
         <div className="mt-[40px] w-full items-center">
